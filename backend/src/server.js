7ae84ce8066f3ce961181edc66cdc8fc2178dbbf
Emir <admin@MacBook-Air--Emir.local>
2024-05-22 20:28:29 +0600

    files for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv';
 dotenv.config();
+import {fileURLToPath} from 'url';
 
 import express from 'express';
 import cors from 'cors';
@@ -8,7 +9,11 @@ import userRouter from './routers/user.router.js';
 import orderRouter from './routers/order.router.js';
 
 import { dbconnect } from './config/database.config.js';
+import path, { dirname } from 'path';
 dbconnect();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 app.use(express.json());
@@ -21,6 +26,14 @@ app.use(
 app.use('/api/foods', foodRouter);
 app.use('/api/users', userRouter);
 app.use('/api/orders', orderRouter);
+
+const publicFolder = path.join(__dirname, 'public');
+app.use(express.static(publicFolder));
+
+app.get('*', (req, res) => {
+  const indexFilePath = path.join(publicFolder, 'index.html');
+  res.sendFile(indexFilePath);
+})
 
 const PORT = 5001;
 app.listen(PORT, () => {
